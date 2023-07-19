7eb03f3bbb405004271de370d21bab646c9f4dea
Daminda Jayaratne <damindda@gmail.com>
2023-07-19 10:40:48 +0100

    chore: style changes

--- src/components/top-navbar/Topnavbar.tsx
+++ src/components/top-navbar/Topnavbar.tsx
@@ -2,13 +2,20 @@ import './topnavbar.scss';
 
 const TopNavbar = () => {
   return (
-    <div className="top-navbar flex">
-      <div className="logo flex">LOGO</div>
-      <ul className="flex">
-        <li>search</li>
-        <li>dashboard</li>
-        <li>notifications</li>
-      </ul>
+    <div className="top-navbar flex align-center space-between">
+      <div className="logo-container flex align-center ">
+        <img src="/public/favicon.svg" className="logo" /> <label className="px-10 text-white">HR Dashboard</label>
+      </div>
+      <div className="top-nav-items flex">
+        <ul className="flex">
+          <li>search</li>
+          <li>dashboard</li>
+          <li>notifications</li>
+        </ul>
+        <div className="user-settings flex align-center text-white">
+          <label>David Walker</label>
+        </div>
+      </div>
     </div>
   );
 };
