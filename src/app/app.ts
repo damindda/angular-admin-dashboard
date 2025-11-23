355330b2ff1fabd0863b8484e54a7d0b90ef4a7e
Daminda Jayaratne <damindda@gmail.com>
2025-11-23 20:08:30 +0000

    chore: router setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Comments } from './components/comments';
 
 @Component({
-	selector: "app-root",
-	imports: [RouterOutlet],
-	templateUrl: "./app.html",
-	styleUrl: "./app.css",
+  imports: [RouterOutlet, Comments],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {
-	protected readonly title = signal("angular-admin-dashboard");
+  protected readonly title = signal('angular-admin-dashboard');
 }
---
+++ src/app/components/comments.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'comments',
+  template: ` <h1>Admin Template</h1> `,
+})
+export class Comments {}
