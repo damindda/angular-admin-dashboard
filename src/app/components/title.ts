178c4f6cf303cab7d44d8c2a62064c03f0ba7860
Daminda Jayaratne <damindda@gmail.com>
2025-11-24 11:17:35 +0000

    chore: spacing added

--- src/app/components/title.ts
+++ src/app/components/title.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <h1
-      class="p-4 text-transparent bg-clip-text bg-linear-to-r to-emerald-600 from-sky-400 text-2xl md:text-3xl lg:text-4xl"
+      class="text-transparent font-bold bg-clip-text bg-linear-to-r to-emerald-200 from-sky-800 text-2xl md:text-3xl lg:text-4xl"
     >
       {{ headerText() }}
     </h1>
