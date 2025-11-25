b821b4acbd0311fcb486231cb33c2fda84262ae9
Daminda Jayaratne <damindda@gmail.com>
2025-11-25 08:17:54 +0000

    chore: left nav styles

--- src/app/components/search-bar/toggle-menu.ts
+++ src/app/components/search-bar/toggle-menu.ts
@@ -1,18 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'bee-toggle-menu',
-  imports: [NgClass],
   template: `
-    <button
-      (click)="toggleMenu()"
-      [ngClass]="
-        selected() ? 'lg:bg-transparent dark:lg:bg-transparent bg-gray-100 dark:bg-gray-800' : ''
-      "
-      class="z-99999 flex h-10 w-10 items-center justify-center rounded-lg border-black/20 text-gray-500 lg:h-11 lg:w-11 lg:border"
-    >
-      <img src="images/svgs/header-menu/menu.svg" class="w-6 h-6" alt="" />
+    <button class="relative group" (click)="toggleMenu()">
+      <div
+        class="relative flex overflow-hidden w-[50px] h-[50px] transform transition-all duration-200 items-center justify-center rounded-lg border-black/20 text-gray-500 lg:h-11 lg:w-11 lg:border"
+      >
+        <div
+          class="flex flex-col justify-between w-5 h-5 transform transition-all duration-500 origin-center overflow-hidden group-focus:-rotate-180"
+        >
+          <div
+            class="bg-black h-0.5 w-7 transform transition-all duration-500 group-focus:rotate-45 -translate-x-1"
+          ></div>
+          <div class="bg-black h-0.5 w-7 rounded transform transition-all duration-500 "></div>
+          <div
+            class="bg-black h-0.5 w-7 transform transition-all duration-500 group-focus:-rotate-45 -translate-x-1"
+          ></div>
+        </div>
+      </div>
     </button>
   `,
 })
